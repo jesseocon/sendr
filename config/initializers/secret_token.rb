8bd26b356d822486f4db4f3dbbade0f421d90e24
jesseocon <jesse.ocon@oconcompany.com>
2013-09-14 20:39:05 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sendr::Application.config.secret_token = 'b7a5664129e8b7343f25f3b4ed12ac871819d6c2a4b5fdc026d7e8a3ef1b45fce34e5732e6625fcb3aadd7543f0f483efe494e635bc124775b926595fe7502c6'
