613d4a1f238ffa306976ad128f15a55a99e63663
jesseocon <jesse.ocon@oconcompany.com>
2013-09-21 15:42:14 -0700

    added api versioning to the application

---
+++ app/controllers/api/v1/tasks_controller.rb
@@ -0,0 +1,44 @@
+module Api
+  module V1
+    class TasksController < ApplicationController
+      respond_to :json
+      def index
+
+      end
+
+      def new
+        @task = Task.new()
+      end
+
+      def create
+        @error = { error: "your task could not be created" }
+        @task = Task.new(command_url: CommandUrl.new(url_string: params[:url]))
+        @task.url = params[:url] if params[:url]
+        @task.exec_time = params[:exec_time]
+        @task.method = params[:method] if params[:method]
+        if @task.save
+          @task.in_the_future
+          respond_with(@task)
+        else
+          respond_with(@error)
+        end
+      end
+
+      def edit
+
+      end
+
+      def update
+
+      end
+
+      def show 
+
+      end
+
+      def destroy
+
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Sendr::Application.routes.draw do
   resources :tasks
   resources :tests
+  ## JSON API
+  namespace :api, defaults: { format: 'json'} do
+    namespace :v1 do
+      resources :tasks
+    end
+  end
 end
